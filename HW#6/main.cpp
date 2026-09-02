6b1927453b1c9d0b685530130afe970483c0a8ff
niel0258 <67369369+niel0258@users.noreply.github.com>
2026-09-02 19:20:10 +0800

    idk what changed

--- HW#6/main.cpp
+++ HW#6/main.cpp
@@ -1,57 +1,13 @@
-#include <iostream>
-#include <cstdlib>//random gen
-#include <time.h>//seed
-
 //#include "node.h"
+#include "node.h"
 #include "searchingAlgo.h"
 
 const int max_size = 10;
 
-void addRandomMembers(int dataset[]){
-    srand(time(0));
-    std::cout << "[";
-    for (int i = 0; i < max_size;i++){
-        dataset[i] = rand();
-        std::cout << dataset[i]<<",";
-    }
-    std::cout << "]\n";
-}
-
 int main(){
-    /*
-    int dataset[max_size] = {1,2,3,4,5,6,7,8,9,10};
-    //addRandomMembers(dataset);
+    int myList[max_size] = {3, 5, 6, 8, 11, 12, 14, 15, 17, 18}; 
 
-    int itemToFind;
-    std::cout << "Enter data to find:\n";
-    std::cin >> itemToFind;
-
-    //linearSearch(max_size,itemToFind,dataset);
-    binarySearchArr(dataset, max_size, itemToFind);
-    */
-
-    Node<int> *num1  = newNode(10);
-    Node<int> *num2  = newNode(20);
-    Node<int> *num3  = newNode(30);
-    Node<int> *num4  = newNode(40);
-    Node<int> *num5  = newNode(50);
-    Node<int> *num6  = newNode(60);
-    Node<int> *num7  = newNode(70);
-    Node<int> *num8  = newNode(80);
-    Node<int> *num9  = newNode(90);
-    Node<int> *num10 = newNode(100);
-
-    num1->next  = num2;
-    num2->next  = num3;
-    num3->next  = num4;
-    num4->next  = num5;
-    num5->next  = num6;
-    num6->next  = num7;
-    num7->next  = num8;
-    num8->next  = num9;
-    num9->next  = num10;
-
-    binarySearchLL(num1, 66);
-
+    binarySearchArr(myList, max_size, 8);
+    //binarySearchArrRec(myList, 0,max_size-1 ,20);
     return 0;
 }
--- HW#6/searchingAlgo.h
+++ HW#6/searchingAlgo.h
@@ -8,49 +8,84 @@
 
 template<typename T>
 void linearSearch(int size,T item,T array[]){
+    //int comparisons = 0;
+    int count = 0;
     for (int i = 0; i < size;i++){
+        //std::cout << "Comparisons made:" << ++comparisons << '\n';
         if(array[i] == item){
             std::cout << "Data Found at index " << i << "\n";
-            return;
+            std::cout << "The same element found: " << ++count << '\n';
+            //return;
         }
+    }
+    if (count > 0) {
+        return;
     }
     std::cout << "Data not found\n";
 }
 
 template<typename T>
-void binarySearchArr(T data[],int arrSize,T dataFind){
+void binarySearchArr(T arr[],int arrSize,T dataFind){
     int low = 0;
     int high = arrSize - 1;
     int middle;
 
     while (low <= high){
         middle = (int)((low+high)/2);
-        if (dataFind == data[middle]){
+        std::cout << "Middle index: " << middle << " , element: " << arr[middle] << '\n';
+        if (dataFind == arr[middle]){
             std::cout << "The data was found\n";
             return;
         }
-        else if (dataFind < data[middle]){
+        else if (dataFind < arr[middle]){
             high = middle - 1;
+            std::cout << "less than middle, searching the left half, high: " << high << '\n' ;
         }
         else {
             low = middle + 1;
+            std::cout << "greater than middle, searching the right half, low: " << low << '\n' ;
         }
     }
 
     std::cout << "The data was not found\n";
 }
 
+template<typename T>
+void binarySearchArrRec(T arr[],int low,int high,T dataFind){
+    if (low > high){
+        std::cout << "Data not found"<<'\n';
+        return;
+    }
+    int middle = (int)((low+high)/2);
+    if (dataFind == arr[middle]){
+        std::cout << "Element found at index: " << middle << '\n';
+        return;
+    }
+    if (dataFind < arr[middle]){
+        binarySearchArrRec(arr, low, middle - 1, dataFind);
+    }
+    else{
+        binarySearchArrRec(arr, middle+1, high, dataFind);
+    }
+}
 //===================================================================
 
 //=============================== LINKED LIST ================================
 template<typename T>
 void linearSearchLL(Node<T> *headNode, T dataToBeFound){
+    //int comparisons = 0;
+    int count = 0;
     while (headNode != nullptr){
+        //std::cout << "Comparisons made:" << ++comparisons << '\n';
         if (headNode->data == dataToBeFound){
             std::cout << "Found Data inside the node\n";
-            return;
+            std::cout << "The same element found: " << ++count << '\n';
+            //return;
         }
         headNode = headNode->next;
+    }
+    if (count > 0) {
+        return;
     }
     std::cout << "Data not found in nodes given\n";
 }
